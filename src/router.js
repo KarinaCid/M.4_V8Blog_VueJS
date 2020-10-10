0e13ec7794b80652586bdac85880d9ea55d9ef5f
Karina Cid <memento_mori.06@hotmail.com>
2020-10-09 23:32:45 -0300

    changes in components & router

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import SobreMi from './components/SobreMi'
 import Contacto from './components/Contacto'
 import NotFound from './components/NotFound'
 import RecentPost from './components/RecentPost'
+import Administrador from './components/Administrador'
 
 Vue.use(Router)
 
@@ -19,12 +20,19 @@ export default new Router({
         {
             path: '/sobremi',
             name: 'sobremi',
-            component: SobreMi
+            component: SobreMi,
+            alias: ['/acerca']
+
+        },
+        {
+            path: '/contactame',
+            redirect: {name: 'contacto'}
         },
         {
             path: '/contacto',
             name: 'contacto',
-            component: Contacto
+            component: Contacto,
+
         },
         {
             path: '/post/:number',
@@ -35,6 +43,12 @@ export default new Router({
             path: '*',
             name: 'NotFound',
             component: NotFound,
+        },
+        {
+            path: '/administrador/:admin',
+            name: 'Administrador',
+            component: Administrador,
+            props: true
         }
     ]
 })
